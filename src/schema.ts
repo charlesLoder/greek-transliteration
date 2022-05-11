c4546b686b3591968c879bea2ea31374339d62fe
Charles Loder <cloder@classicalsubjects.com>
2022-05-11 19:02:56 -0400

    Create schema

---
+++ src/schema.ts
@@ -0,0 +1,604 @@
+interface SchemaOptions {
+  /**
+   * GREEK CAPITAL LETTER ALPHA Α
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "A"
+   */
+  CAPITAL_ALPHA?: string;
+  /**
+   * GREEK CAPITAL LETTER BETA Β
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "B"
+   */
+  CAPITAL_BETA?: string;
+  /**
+   * GREEK CAPITAL LETTER GAMMA Γ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "G"
+   */
+  CAPITAL_GAMMA?: string;
+  /**
+   * GREEK CAPITAL LETTER DELTA Δ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "D"
+   */
+  CAPITAL_DELTA?: string;
+  /**
+   * GREEK CAPITAL LETTER EPSILON Ε
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "E"
+   */
+  CAPITAL_EPSILON?: string;
+  /**
+   * GREEK CAPITAL LETTER ZETA Ζ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "Z"
+   */
+  CAPITAL_ZETA?: string;
+  /**
+   * GREEK CAPITAL LETTER ETA Η
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "Ē"
+   */
+  CAPITAL_ETA?: string;
+  /**
+   * GREEK CAPITAL LETTER THETA Θ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "TH"
+   */
+  CAPITAL_THETA?: string;
+  /**
+   * GREEK CAPITAL LETTER IOTA Ι
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "I"
+   */
+  CAPITAL_IOTA?: string;
+  /**
+   * GREEK CAPITAL LETTER KAPPA Κ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "K"
+   */
+  CAPITAL_KAPPA?: string;
+  /**
+   * GREEK CAPITAL LETTER LAMDA Λ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "L"
+   */
+  CAPITAL_LAMDA?: string;
+  /**
+   * GREEK CAPITAL LETTER MU Μ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "M"
+   */
+  CAPITAL_MU?: string;
+  /**
+   * GREEK CAPITAL LETTER NU Ν
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "N"
+   */
+  CAPITAL_NU?: string;
+  /**
+   * GREEK CAPITAL LETTER XI Ξ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "X"
+   */
+  CAPITAL_XI?: string;
+  /**
+   * GREEK CAPITAL LETTER OMICRON Ο
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "O"
+   */
+  CAPITAL_OMICRON?: string;
+  /**
+   * GREEK CAPITAL LETTER PI Π
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form; if false, defaults to small form
+   * @example
+   * "P"
+   */
+  CAPITAL_PI?: string;
+  /**
+   * GREEK CAPITAL LETTER RHO Ρ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "R"
+   */
+  CAPITAL_RHO?: string;
+  /**
+   * GREEK CAPITAL LETTER SIGMA Σ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "S"
+   */
+  CAPITAL_SIGMA?: string;
+  /**
+   * GREEK CAPITAL LETTER TAU Τ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "T"
+   */
+  CAPITAL_TAU?: string;
+  /**
+   * GREEK CAPITAL LETTER UPSILON Υ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "Y"
+   */
+  CAPITAL_UPSILON?: string;
+  /**
+   * GREEK CAPITAL LETTER PHI Φ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "PH"
+   */
+  CAPITAL_PHI?: string;
+  /**
+   * GREEK CAPITAL LETTER CHI Χ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "CH"
+   */
+  CAPITAL_CHI?: string;
+  /**
+   * GREEK CAPITAL LETTER PSI Ψ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "PS"
+   */
+  CAPITAL_PSI?: string;
+  /**
+   * GREEK CAPITAL LETTER OMEGA Ω
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "Ō"
+   */
+  CAPITAL_OMEGA?: string;
+
+  /**
+   * GREEK SMALL LETTER ALPHA α
+   * @example
+   * "a"
+   */
+  SMALL_ALPHA: string;
+  /**
+   * GREEK SMALL LETTER BETA β
+   * @example
+   * "b"
+   */
+  SMALL_BETA: string;
+  /**
+   * GREEK SMALL LETTER GAMMA γ
+   * @example
+   * "g"
+   */
+  SMALL_GAMMA: string;
+  /**
+   * GREEK SMALL LETTER DELTA δ
+   * @example
+   * "d"
+   */
+  SMALL_DELTA: string;
+  /**
+   * GREEK SMALL LETTER EPSILON ε
+   * @example
+   * "e"
+   */
+  SMALL_EPSILON: string;
+  /**
+   * GREEK SMALL LETTER ZETA ζ
+   * @example
+   * "z"
+   */
+  SMALL_ZETA: string;
+  /**
+   * GREEK SMALL LETTER ETA η
+   * @example
+   * "ē"
+   */
+  SMALL_ETA: string;
+  /**
+   * GREEK SMALL LETTER THETA θ
+   * @example
+   * "th"
+   */
+  SMALL_THETA: string;
+  /**
+   * GREEK SMALL LETTER IOTA ι
+   * @example
+   * "i"
+   */
+  SMALL_IOTA: string;
+  /**
+   * GREEK SMALL LETTER KAPPA κ
+   * @example
+   * "k"
+   */
+  SMALL_KAPPA: string;
+  /**
+   * GREEK SMALL LETTER LAMDA λ
+   * @example
+   * "l"
+   */
+  SMALL_LAMDA: string;
+  /**
+   * GREEK SMALL LETTER MU μ
+   * @example
+   * "m"
+   */
+  SMALL_MU: string;
+  /**
+   * GREEK SMALL LETTER NU ν
+   * @example
+   * "n"
+   */
+  SMALL_NU: string;
+  /**
+   * GREEK SMALL LETTER XI ξ
+   * @example
+   * "x"
+   */
+  SMALL_XI: string;
+  /**
+   * GREEK SMALL LETTER OMICRON ο
+   * @example
+   * "o"
+   */
+  SMALL_OMICRON: string;
+  /**
+   * GREEK SMALL LETTER PI π
+   * @example
+   * "p"
+   */
+  SMALL_PI: string;
+  /**
+   * GREEK SMALL LETTER RHO ρ
+   * @example
+   * "r"
+   */
+  SMALL_RHO: string;
+  /**
+   * GREEK SMALL LETTER FINAL SIGMA ς
+   * @example
+   * "s"
+   */
+  SMALL_FINAL_SIGMA: string;
+  /**
+   * GREEK SMALL LETTER SIGMA σ
+   * @example
+   * "s"
+   */
+  SMALL_SIGMA: string;
+  /**
+   * GREEK SMALL LETTER TAU τ
+   * @example
+   * "t"
+   */
+  SMALL_TAU: string;
+  /**
+   * GREEK SMALL LETTER UPSILON υ
+   * @example
+   * "y"
+   */
+  SMALL_UPSILON: string;
+  /**
+   * GREEK SMALL LETTER PHI φ
+   * @example
+   * "ph"
+   */
+  SMALL_PHI: string;
+  /**
+   * GREEK SMALL LETTER CHI χ
+   * @example
+   * "ch"
+   */
+  SMALL_CHI: string;
+  /**
+   * GREEK SMALL LETTER PSI ψ
+   * @example
+   * "ps"
+   */
+  SMALL_PSI: string;
+  /**
+   * GREEK SMALL LETTER OMEGA ω
+   * @example
+   * "ō"
+   */
+  SMALL_OMEGA: string;
+  /**
+   * GREEK SMALL GAMMA Nasal
+   * @remarks
+   * a GAMMA followed by GAMMA, KAPPA, XI, or CHI (e.g. γγ, γκ, γξ, γχ)
+   * @example
+   * "n"
+   */
+  SMALL_GAMMA_NASAL: string;
+  /**
+   * GREEK CAPTIAL GAMMA Nasal
+   * @remarkd
+   * a GAMMA followed by GAMMA, KAPPA, XI, or CHI (e.g. ΓΓ, ΓΚ, ΓΞ, ΓΧ)
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "N"
+   */
+  CAPITAL_GAMMA_NASAL?: string;
+  /**
+   * GREEK SMALL RHO followed by RHO ρρ
+   * @example
+   * "rrh"
+   */
+  SMALL_DOUBLE_RHO: string;
+  /**
+   * GREEK CAPITAL RHO followed by RHO ΡΡ
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "RRH"
+   */
+  CAPITAL_DOUBLE_RHO?: string;
+  /**
+   * GREEK SMALL UPSILON in a diphthong υ
+   * @remarks
+   * when a upsilon is used in a diphthong (e.g. αυ, ευ, ηυ, ου, υι)
+   * @example
+   * "u"
+   */
+  SMALL_UPSILON_DIPTHONG: string;
+  /**
+   * GREEK CAPITAL UPSILON in a diphthong Υ
+   * @remarks
+   * when a upsilon is used in a diphthong (e.g. ΑΥ, ΕΥ, ΗΥ, ΟΥ, ΥΙ)
+   * @remarks
+   * if `undefined`, defaults to small form, and if `preserveCapitals` true, defaults to uppercase of small form
+   * @example
+   * "U"
+   */
+  CAPITAL_UPSILON_DIPTHONG?: string;
+  /**
+   * GREEK DASIA  ̔
+   * @remarks
+   * also called U+0314 COMBINING REVERSED COMMA ABOVE
+   * @example
+   * "h"
+   */
+  ROUGH_BREATHING_MARK: string;
+  /**
+   * if false, converts all characters to lowercase
+   */
+  preserveCapitals?: boolean;
+}
+
+/**
+ * class for defining a schema for transliteration
+ */
+export class Schema {
+  CAPITAL_ALPHA: string;
+  CAPITAL_BETA: string;
+  CAPITAL_GAMMA: string;
+  CAPITAL_DELTA: string;
+  CAPITAL_EPSILON: string;
+  CAPITAL_ZETA: string;
+  CAPITAL_ETA: string;
+  CAPITAL_THETA: string;
+  CAPITAL_IOTA: string;
+  CAPITAL_KAPPA: string;
+  CAPITAL_LAMDA: string;
+  CAPITAL_MU: string;
+  CAPITAL_NU: string;
+  CAPITAL_XI: string;
+  CAPITAL_OMICRON: string;
+  CAPITAL_PI: string;
+  CAPITAL_RHO: string;
+  CAPITAL_SIGMA: string;
+  CAPITAL_TAU: string;
+  CAPITAL_UPSILON: string;
+  CAPITAL_PHI: string;
+  CAPITAL_CHI: string;
+  CAPITAL_PSI: string;
+  CAPITAL_OMEGA: string;
+  SMALL_ALPHA: string;
+  SMALL_BETA: string;
+  SMALL_GAMMA: string;
+  SMALL_DELTA: string;
+  SMALL_EPSILON: string;
+  SMALL_ZETA: string;
+  SMALL_ETA: string;
+  SMALL_THETA: string;
+  SMALL_IOTA: string;
+  SMALL_KAPPA: string;
+  SMALL_LAMDA: string;
+  SMALL_MU: string;
+  SMALL_NU: string;
+  SMALL_XI: string;
+  SMALL_OMICRON: string;
+  SMALL_PI: string;
+  SMALL_RHO: string;
+  SMALL_FINAL_SIGMA: string;
+  SMALL_SIGMA: string;
+  SMALL_TAU: string;
+  SMALL_UPSILON: string;
+  SMALL_PHI: string;
+  SMALL_CHI: string;
+  SMALL_PSI: string;
+  SMALL_OMEGA: string;
+  SMALL_GAMMA_NASAL: string;
+  CAPITAL_GAMMA_NASAL: string;
+  SMALL_DOUBLE_RHO: string;
+  CAPITAL_DOUBLE_RHO: string;
+  SMALL_UPSILON_DIPTHONG: string;
+  CAPITAL_UPSILON_DIPTHONG: string;
+  ROUGH_BREATHING_MARK: string;
+  preserveCapitals: boolean;
+  constructor(schema: SchemaOptions) {
+    // uppercase
+    this.CAPITAL_ALPHA = schema.CAPITAL_ALPHA || schema.SMALL_ALPHA.toUpperCase();
+    this.CAPITAL_BETA = schema.CAPITAL_BETA || schema.SMALL_BETA.toUpperCase();
+    this.CAPITAL_GAMMA = schema.CAPITAL_GAMMA || schema.SMALL_GAMMA.toUpperCase();
+    this.CAPITAL_DELTA = schema.CAPITAL_DELTA || schema.SMALL_DELTA.toUpperCase();
+    this.CAPITAL_EPSILON = schema.CAPITAL_EPSILON || schema.SMALL_EPSILON.toUpperCase();
+    this.CAPITAL_ZETA = schema.CAPITAL_ZETA || schema.SMALL_ZETA.toUpperCase();
+    this.CAPITAL_ETA = schema.CAPITAL_ETA || schema.SMALL_ETA.toUpperCase();
+    this.CAPITAL_THETA = schema.CAPITAL_THETA || schema.SMALL_THETA.toUpperCase();
+    this.CAPITAL_IOTA = schema.CAPITAL_IOTA || schema.SMALL_IOTA.toUpperCase();
+    this.CAPITAL_KAPPA = schema.CAPITAL_KAPPA || schema.SMALL_KAPPA.toUpperCase();
+    this.CAPITAL_LAMDA = schema.CAPITAL_LAMDA || schema.SMALL_LAMDA.toUpperCase();
+    this.CAPITAL_MU = schema.CAPITAL_MU || schema.SMALL_MU.toUpperCase();
+    this.CAPITAL_NU = schema.CAPITAL_NU || schema.SMALL_NU.toUpperCase();
+    this.CAPITAL_XI = schema.CAPITAL_XI || schema.SMALL_XI.toUpperCase();
+    this.CAPITAL_OMICRON = schema.CAPITAL_OMICRON || schema.SMALL_OMICRON.toUpperCase();
+    this.CAPITAL_PI = schema.CAPITAL_PI || schema.SMALL_PI.toUpperCase();
+    this.CAPITAL_RHO = schema.CAPITAL_RHO || schema.SMALL_RHO.toUpperCase();
+    this.CAPITAL_SIGMA = schema.CAPITAL_SIGMA || schema.SMALL_SIGMA.toUpperCase();
+    this.CAPITAL_TAU = schema.CAPITAL_TAU || schema.SMALL_TAU.toUpperCase();
+    this.CAPITAL_UPSILON = schema.CAPITAL_UPSILON || schema.SMALL_UPSILON.toUpperCase();
+    this.CAPITAL_PHI = schema.CAPITAL_PHI || schema.SMALL_PHI.toUpperCase();
+    this.CAPITAL_CHI = schema.CAPITAL_CHI || schema.SMALL_CHI.toUpperCase();
+    this.CAPITAL_PSI = schema.CAPITAL_PSI || schema.SMALL_PSI.toUpperCase();
+    this.CAPITAL_OMEGA = schema.CAPITAL_OMEGA || schema.SMALL_OMEGA.toUpperCase();
+    // lowercase
+    this.SMALL_ALPHA = schema.SMALL_ALPHA;
+    this.SMALL_BETA = schema.SMALL_BETA;
+    this.SMALL_GAMMA = schema.SMALL_GAMMA;
+    this.SMALL_DELTA = schema.SMALL_DELTA;
+    this.SMALL_EPSILON = schema.SMALL_EPSILON;
+    this.SMALL_ZETA = schema.SMALL_ZETA;
+    this.SMALL_ETA = schema.SMALL_ETA;
+    this.SMALL_THETA = schema.SMALL_THETA;
+    this.SMALL_IOTA = schema.SMALL_IOTA;
+    this.SMALL_KAPPA = schema.SMALL_KAPPA;
+    this.SMALL_LAMDA = schema.SMALL_LAMDA;
+    this.SMALL_MU = schema.SMALL_MU;
+    this.SMALL_NU = schema.SMALL_NU;
+    this.SMALL_XI = schema.SMALL_XI;
+    this.SMALL_OMICRON = schema.SMALL_OMICRON;
+    this.SMALL_PI = schema.SMALL_PI;
+    this.SMALL_RHO = schema.SMALL_RHO;
+    this.SMALL_FINAL_SIGMA = schema.SMALL_FINAL_SIGMA;
+    this.SMALL_SIGMA = schema.SMALL_SIGMA;
+    this.SMALL_TAU = schema.SMALL_TAU;
+    this.SMALL_UPSILON = schema.SMALL_UPSILON;
+    this.SMALL_PHI = schema.SMALL_PHI;
+    this.SMALL_CHI = schema.SMALL_CHI;
+    this.SMALL_PSI = schema.SMALL_PSI;
+    this.SMALL_OMEGA = schema.SMALL_OMEGA;
+    // orthographies
+    this.SMALL_GAMMA_NASAL = schema.SMALL_GAMMA_NASAL || schema.SMALL_GAMMA;
+    this.CAPITAL_GAMMA_NASAL = schema.CAPITAL_GAMMA_NASAL || this.SMALL_GAMMA_NASAL.toUpperCase();
+    this.SMALL_DOUBLE_RHO = schema.SMALL_DOUBLE_RHO;
+    this.CAPITAL_DOUBLE_RHO = schema.CAPITAL_DOUBLE_RHO || schema.SMALL_DOUBLE_RHO.toUpperCase();
+    this.SMALL_UPSILON_DIPTHONG = schema.SMALL_UPSILON_DIPTHONG;
+    this.CAPITAL_UPSILON_DIPTHONG = schema.CAPITAL_UPSILON_DIPTHONG || this.SMALL_UPSILON_DIPTHONG.toUpperCase();
+    this.ROUGH_BREATHING_MARK = schema.ROUGH_BREATHING_MARK;
+    // functionality
+    this.preserveCapitals = schema.preserveCapitals ?? true;
+  }
+}
+
+/**
+ * defaul SBL Academic schema
+ */
+export class SBL extends Schema {
+  constructor(schema: Partial<Schema>) {
+    super({
+      // uppercase
+      CAPITAL_ALPHA: schema.CAPITAL_ALPHA || "A",
+      CAPITAL_BETA: schema.CAPITAL_BETA || "B",
+      CAPITAL_GAMMA: schema.CAPITAL_GAMMA || "G",
+      CAPITAL_DELTA: schema.CAPITAL_DELTA || "D",
+      CAPITAL_EPSILON: schema.CAPITAL_EPSILON || "E",
+      CAPITAL_ZETA: schema.CAPITAL_ZETA || "Z",
+      CAPITAL_ETA: schema.CAPITAL_ETA || "Ē",
+      CAPITAL_THETA: schema.CAPITAL_THETA || "TH",
+      CAPITAL_IOTA: schema.CAPITAL_IOTA || "I",
+      CAPITAL_KAPPA: schema.CAPITAL_KAPPA || "K",
+      CAPITAL_LAMDA: schema.CAPITAL_LAMDA || "L",
+      CAPITAL_MU: schema.CAPITAL_MU || "M",
+      CAPITAL_NU: schema.CAPITAL_NU || "N",
+      CAPITAL_XI: schema.CAPITAL_XI || "X",
+      CAPITAL_OMICRON: schema.CAPITAL_OMICRON || "O",
+      CAPITAL_PI: schema.CAPITAL_PI || "P",
+      CAPITAL_RHO: schema.CAPITAL_RHO || "R",
+      CAPITAL_SIGMA: schema.CAPITAL_SIGMA || "S",
+      CAPITAL_TAU: schema.CAPITAL_TAU || "T",
+      CAPITAL_UPSILON: schema.CAPITAL_UPSILON || "Y",
+      CAPITAL_PHI: schema.CAPITAL_PHI || "PH",
+      CAPITAL_CHI: schema.CAPITAL_CHI || "CH",
+      CAPITAL_PSI: schema.CAPITAL_PSI || "PS",
+      CAPITAL_OMEGA: schema.CAPITAL_OMEGA || "Ō",
+      // lowercase
+      SMALL_ALPHA: schema.SMALL_ALPHA || "a",
+      SMALL_BETA: schema.SMALL_BETA || "b",
+      SMALL_GAMMA: schema.SMALL_GAMMA || "g",
+      SMALL_DELTA: schema.SMALL_DELTA || "d",
+      SMALL_EPSILON: schema.SMALL_EPSILON || "e",
+      SMALL_ZETA: schema.SMALL_ZETA || "z",
+      SMALL_ETA: schema.SMALL_ETA || "ē",
+      SMALL_THETA: schema.SMALL_THETA || "th",
+      SMALL_IOTA: schema.SMALL_IOTA || "i",
+      SMALL_KAPPA: schema.SMALL_KAPPA || "k",
+      SMALL_LAMDA: schema.SMALL_LAMDA || "l",
+      SMALL_MU: schema.SMALL_MU || "m",
+      SMALL_NU: schema.SMALL_NU || "n",
+      SMALL_XI: schema.SMALL_XI || "x",
+      SMALL_OMICRON: schema.SMALL_OMICRON || "o",
+      SMALL_PI: schema.SMALL_PI || "p",
+      SMALL_RHO: schema.SMALL_RHO || "r",
+      SMALL_FINAL_SIGMA: schema.SMALL_FINAL_SIGMA || "s",
+      SMALL_SIGMA: schema.SMALL_SIGMA || "s",
+      SMALL_TAU: schema.SMALL_TAU || "t",
+      SMALL_UPSILON: schema.SMALL_UPSILON || "y",
+      SMALL_PHI: schema.SMALL_PHI || "ph",
+      SMALL_CHI: schema.SMALL_CHI || "ch",
+      SMALL_PSI: schema.SMALL_PSI || "ps",
+      SMALL_OMEGA: schema.SMALL_OMEGA || "ō",
+      // orthographies
+      SMALL_GAMMA_NASAL: schema.SMALL_GAMMA_NASAL || "n",
+      CAPITAL_GAMMA_NASAL: schema.CAPITAL_GAMMA_NASAL || "N",
+      SMALL_DOUBLE_RHO: schema.SMALL_DOUBLE_RHO || "rrh",
+      CAPITAL_DOUBLE_RHO: schema.CAPITAL_DOUBLE_RHO || "RRH",
+      SMALL_UPSILON_DIPTHONG: schema.SMALL_UPSILON_DIPTHONG || "u",
+      CAPITAL_UPSILON_DIPTHONG: schema.CAPITAL_UPSILON_DIPTHONG || "U",
+      ROUGH_BREATHING_MARK: schema.ROUGH_BREATHING_MARK || "h",
+      // functionality
+      preserveCapitals: schema.preserveCapitals ?? true
+    });
+  }
+}
