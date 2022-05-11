f4a46b188c2bb6821a087e09c5ce0d719173da89
Charles Loder <cloder@classicalsubjects.com>
2022-05-11 19:03:37 -0400

    Update transliterate

--- src/transliterate.ts
+++ src/transliterate.ts
@@ -1,22 +1,23 @@
-import { titForTat } from "./titForTat";
-import { testEach } from "./testEach";
-
-interface Options {
-  preserveCapitals: boolean;
-}
+import { rules } from "./testEach";
+import { Schema, SBL } from "./schema";
 
 /**
- *
- * @param {string} text
- * @param {Options} options
- * @returns {string} tansliteration of text
+ * transliterates Greek text according to Schema
+ * @param {string} text - Greek text to be transliterated
+ * @param {Schema} schema - if `undefined`, defaults to SBL Academic
+ * @returns {string} transliteration of text
  */
-export const transliterate = (text: string, options: Options = { preserveCapitals: false }) => {
+export const transliterate = (text: string, schema?: Partial<Schema> | Schema) => {
+  const transSchema = schema instanceof Schema ? schema : new SBL(schema ?? {});
   const normalize = text.normalize("NFKD");
-  const textCase = options.preserveCapitals ? normalize : normalize.toLowerCase();
-  const titTat = titForTat(textCase);
-  const array = titTat.split(" ");
-  const modArray = testEach(array);
-  const transliteration = modArray.join(" ");
-  return transliteration;
+  // removes unused characters from NFKD normalization
+  const unused = /[\u{00B7}\u{0300}\u{0301}\u{0304}\u{0306}\u{0313}\u{0342}\u{0345}]/gu;
+  const sanitized = normalize.replace(unused, "");
+  const textCase = transSchema.preserveCapitals ? sanitized : sanitized.toLowerCase();
+  return textCase
+    .split(" ")
+    .map((w) => rules(w, transSchema))
+    .join(" ");
 };
+
+export { Schema };
